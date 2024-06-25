156b65039429c72573060e6cf4b93bad5fae6eb9
naisu-dev <118091656+naisu-dev@users.noreply.github.com>
2024-06-25 13:29:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import requests
+import argostranslate.package
+import argostranslate.translate
 
-print((requests.get("http://numbersapi.com/random/trivia").text))
+from_code = "en"
+to_code = "ja"
+
+translatedText = argostranslate.translate.translate(requests.get("http://numbersapi.com/random/trivia").text, from_code, to_code)
+print(translatedText)
